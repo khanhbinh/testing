884cd2dc15061129d4485ce62f8ea069e060e2fd
Binh Khanh Mai <binh.khanh.mai@su.se>
2017-10-17 09:44:09 +0200

    Modified

--- logic.py
+++ logic.py
@@ -1,32 +1,28 @@
-
-def mymax(x,y):
-    if x > y:
-        return x
-    else:
-        return y
-    return
-
-i = 2
-j = 4
-
-#print ("The maximum of", i, "and", j, "is", mymax(i,j),"\n")
-#print ('The maximum of %d and %d is %d' %(i, j, mymax(i,j)))
-print ("The maximum of {} and {} is {}".format(i, j, mymax(i,j)))
+def mymax(x, y):
+    '''
+    Return the max of two parameters
+    '''
+    return listmax([x, y])
 
 def listmax(l):
     lmax = 0
     for i in l:
-        print (i)
         if i > lmax:
             lmax = i
-        return lmax
-        #if i > lmax:
-        #    lmax = i
-        #return lmax
-      
-
-        
-            
+    return lmax
 
 
+#if __name__ == "__main__":
+#    import sys
+#    mylist = [int(arg) for arg in sys.argv[1:]]
+#    print(listmax(mylist))
 
+#print(__name__)
+if __name__ == "__main__":
+    import sys
+    print(sys.argv)
+    myList = []
+    for arg in sys.argv[1:]:
+        myList.append(int(arg))
+    print(myList)
+    print(listmax(myList))
